0ea354fd4d1109a4402d210ffaceb6b439632b57
mattbooker <mattbooker97@gmail.com>
2022-04-16 15:18:16 -0400

    Fixed Scanner visiting last wp twice

--- src/phase_scan.cpp
+++ src/phase_scan.cpp
@@ -3,26 +3,30 @@
 
 #include <geometry_msgs/Point.h>
 
-void PhaseScan::_enter() {
+void PhaseScan::_enter()
+{
     scan_generator.calcSquareWPs();
     return;
 }
 
-void PhaseScan::_exit() {
+void PhaseScan::_exit()
+{
     return;
 }
 
-void PhaseScan::handler() {
+void PhaseScan::handler()
+{
+    geometry_msgs::Point next_wp = scan_generator.nextWP();
 
     // If scanning has finished transition to ended state
-    if (scan_generator.isFinished()) {
+    if (scan_generator.isFinished())
+    {
         ROS_INFO("Scan path is finished");
         is_transition_needed_ = true;
-        next_phase_type_ =  PhaseType::Ended;
+        next_phase_type_ = PhaseType::Ended;
         return;
     }
 
-    geometry_msgs::Point next_wp = scan_generator.nextWP();
     ROS_INFO("Moving to scanner wp = (%f, %f, %f)", next_wp.x, next_wp.y, next_wp.z);
     sendMoveCommand(next_wp.x, next_wp.y, next_wp.z);
 
