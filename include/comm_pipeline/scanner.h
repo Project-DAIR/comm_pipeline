642447b23d244e58148b18e1d3dcc0efeffb98e5
Nanditha Umasankar <unanditha02@gmail.com>
2022-04-15 23:08:53 -0400

    scanner sqaure calculations fixed

--- include/comm_pipeline/scanner.h
+++ include/comm_pipeline/scanner.h
@@ -15,12 +15,9 @@ public:
 
 private:
     float side_;
-    float scan_diagonal_;
-    float internal_angle_;
     bool is_finished_;
 
     geometry_msgs::Point next_wp_;
-    // std::vector<geometry_msgs::Point> square_WPs_(4, geometry_msgs::Point());
     std::vector<geometry_msgs::Point> square_WPs_;
     std::vector<geometry_msgs::Point>::iterator next_wp_it_;
 };
--- src/scanner.cpp
+++ src/scanner.cpp
@@ -7,25 +7,20 @@
 
 #define PI 3.14159265
 
-Scanner::Scanner() : square_WPs_(5, geometry_msgs::Point()), is_finished_(false)
+Scanner::Scanner() : square_WPs_(6, geometry_msgs::Point()), is_finished_(false)
 {
     ros::NodeHandle param_nh("~");
-    param_nh.param("scan_diagonal", scan_diagonal_, 2.0f);
-
-    internal_angle_ = 45.0 * PI / 180.0;
-
-    side_ = 2 * cos(internal_angle_) * scan_diagonal_;
+    param_nh.param("square_side", side_, 2.0f);
 }
 
 void Scanner::calcSquareWPs()
 {
 
-    square_WPs_[0].x = -scan_diagonal_ * cos(internal_angle_);
-    square_WPs_[0].y = -scan_diagonal_ * sin(internal_angle_);
+    square_WPs_[0].x = 0;
+    square_WPs_[0].y = -side_/2;
     square_WPs_[0].z = 0;
-    // square_WPs_[0] = sq_pose_;
 
-    square_WPs_[1].x = side_;
+    square_WPs_[1].x = side_/2;
     square_WPs_[1].y = 0;
     square_WPs_[1].z = 0;
 
@@ -40,6 +35,10 @@ void Scanner::calcSquareWPs()
     square_WPs_[4].x = 0;
     square_WPs_[4].y = -side_;
     square_WPs_[4].z = 0;
+
+    square_WPs_[5].x = side_;
+    square_WPs_[5].y = 0;
+    square_WPs_[5].z = 0;
 
     next_wp_it_ = square_WPs_.begin();
 
